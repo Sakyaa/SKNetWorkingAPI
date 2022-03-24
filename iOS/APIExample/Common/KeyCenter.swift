e4a976f512e04e3be55eeeab7166eaa62aec18ed
pyfer <pengyunfei@polyv.net>
2022-03-24 11:57:16 +0800

    no message

---
+++ iOS/APIExample/Common/KeyCenter.swift
@@ -0,0 +1,14 @@
+//
+//  KeyCenter.swift
+//  OpenLive
+//
+//  Created by GongYuhua on 6/25/16.
+//  Copyright © 2016 Agora. All rights reserved.
+//
+
+struct KeyCenter {
+    static let AppId: String = "ee6868fc45a248b9b1fbf89e7aaff1e7"
+
+    // assign token to nil if you have not enabled app certificate
+    static var Token: String? = nil
+}
